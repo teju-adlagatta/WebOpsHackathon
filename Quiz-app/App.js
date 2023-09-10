3d73b07c22641533d31533ce7d4d10ce54ce5fee
teju-adlagatta <132986735+teju-adlagatta@users.noreply.github.com>
2023-09-10 11:39:34 +0530

    Update App.js

--- Quiz-app/App.js
+++ Quiz-app/App.js
@@ -1,1 +1,41 @@
-import 
+import FlashcardList from './FlashcardList.js';
+import './app.css';
+import React, {useState} from 'react'; 
+
+function App() {
+  const[flashcards, setFlashcards]= useState(SAMPLE_FLASHCARDS)
+  return (
+   
+   <FlashcardList flashcards={flashcards} />
+
+  );
+}
+const SAMPLE_FLASHCARDS=[
+  {
+    id:1,
+    question: 'what is 2+2?' ,
+    answer: '4',
+    options:[
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  },
+
+  {
+    id:2,
+    question: 'what is 2-2?' ,
+    answer: '0',
+    options:[
+      '1',
+      '2',
+      '3',
+      '0'
+    ]
+  }
+]
+
+export default App;
+
+
